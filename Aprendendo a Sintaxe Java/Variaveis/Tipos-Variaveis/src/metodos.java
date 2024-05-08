953b4ce1b8a7d026979fc7e2c7f5d6a03a881f8f
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-05-08 13:11:41 -0300

    .

---
+++ Aprendendo a Sintaxe Java/Variaveis/Tipos-Variaveis/src/metodos.java
@@ -0,0 +1,5 @@
+public class metodos {
+    public static void main(String[] args) {
+        
+    }
+}
--- Aprendendo a Sintaxe Java/Variaveis/Tipos-Variaveis/src/operadores.java
+++ Aprendendo a Sintaxe Java/Variaveis/Tipos-Variaveis/src/operadores.java
@@ -1,8 +1,34 @@
 public class operadores {
     public static void main(String[] args) {
+        
         String nomeCompleto = "Linguagem" + "Java";
         System.out.println(nomeCompleto);
 
-        
+        //x repeticao 
+
+        int numero =5;
+        numero++;
+        System.out.println(numero);
+
+        boolean variavel = true;
+        variavel = !variavel;
+        System.out.println(variavel);
+
+        int a,b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "Verdadeiro" : "Falso"; //if ternario 
+        System.out.println(resultado);
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeiras");
+        }
     }
 }
