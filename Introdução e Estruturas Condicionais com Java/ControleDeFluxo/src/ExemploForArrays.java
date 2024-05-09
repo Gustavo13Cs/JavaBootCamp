0f45dadd58f8694f264b21cea405c3e54b54820f
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-05-09 12:56:16 -0300

    .

---
+++ Introdução e Estruturas Condicionais com Java/ControleDeFluxo/src/ExemploForArrays.java
@@ -0,0 +1,9 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE","GUSTAVO","JULIA","MARCOS"};
+
+        for(int x=0; x<alunos.length; x++) {
+            System.out.println("o aluno no indice x = " + x + "é " + alunos[ x ]);
+        }
+    }
+}
