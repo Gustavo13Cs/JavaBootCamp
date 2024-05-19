7d47b7c73bfe5b754de0f49a4d759ddc216fa8f0
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-05-19 15:12:55 -0300

    .

--- Conhecendo Collections Java/collections-java-api-2023-master/src/main/java/list/OperacoesBasicas/Tarefa.java
+++ Conhecendo Collections Java/collections-java-api-2023-master/src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -1,19 +1,23 @@
 package main.java.list.OperacoesBasicas;
 
 public class Tarefa {
-  //atributo
-  private String descricao;
+    //atributo 
 
-  public Tarefa(String descricao) {
-    this.descricao = descricao;
-  }
+    private String descricao;
 
-  public String getDescricao() {
-    return descricao;
-  }
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
 
-  @Override
-  public String toString() {
-    return  descricao;
-  }
+    public String getDescricao() {
+        return descricao;
+    }
+
+    //subescrevendo o metodo tostring
+    @Override
+    public String toString() {
+        return "Tarefa{" + 
+        "descricao='" + descricao + '\'' + 
+        '}';
+    }
 }
