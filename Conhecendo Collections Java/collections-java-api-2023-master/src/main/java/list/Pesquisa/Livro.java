19b839f7afdd903a6996b7c5ee2153be3c9f4dda
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-05-19 17:26:15 -0300

    .

--- Conhecendo Collections Java/collections-java-api-2023-master/src/main/java/list/Pesquisa/Livro.java
+++ Conhecendo Collections Java/collections-java-api-2023-master/src/main/java/list/Pesquisa/Livro.java
@@ -1,39 +1,36 @@
 package main.java.list.Pesquisa;
 
 public class Livro {
-  //atributos
-  private String titulo;
-  private String autor;
-  private int anoPublicacao;
+    //atributos 
 
-  public Livro(String titulo, String autor, int anoPublicacao) {
-    this.titulo = titulo;
-    this.autor = autor;
-    this.anoPublicacao = anoPublicacao;
-  }
+    private String titulo;
+    private String autor;
+    private int anoPubicacao;
 
-  public String getTitulo() {
-    return titulo;
-  }
+    public Livro(String titulo,String autor, int anoPubicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPubicacao = anoPubicacao;
+    }
 
-  public void setAutor(String autor) {
-    this.autor = autor;
-  }
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public int getAnoPublicacao() {
+        return anoPubicacao;
+    }
 
-  public String getAutor() {
-    return autor;
-  }
-
-  public int getAnoPublicacao() {
-    return anoPublicacao;
-  }
-
-  @Override
-  public String toString() {
+    @Override
+    public String toString() {
     return "Livro{" +
         "titulo='" + titulo + '\'' +
         ", autor='" + autor + '\'' +
-        ", anoPublicacao=" + anoPublicacao +
+        ", anoPublicacao=" + anoPubicacao +
         '}';
-  }
+    }
+
+
 }
