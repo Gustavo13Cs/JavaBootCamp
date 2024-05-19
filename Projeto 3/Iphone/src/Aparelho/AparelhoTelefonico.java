fa36282ea4f45cdd3b3ea0cf45c7df595f4364e5
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-05-18 21:12:22 -0300

    .

---
+++ Projeto 3/Iphone/src/Aparelho/AparelhoTelefonico.java
@@ -0,0 +1,9 @@
+package Aparelho;
+
+public interface AparelhoTelefonico {
+    public void ligar(String numero);
+
+    public void atender();
+
+    public void IniciarCorreioVoz();
+}
---
+++ Projeto 3/Iphone/src/Iphone.java
@@ -0,0 +1,63 @@
+import Aparelho.AparelhoTelefonico;
+import Navegador.NavegadorInternet;
+import Reprodutor.ReprodutorMusical;
+
+public class Iphone implements AparelhoTelefonico,NavegadorInternet,ReprodutorMusical {
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void tocar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tocar'");
+    }
+
+    @Override
+    public void pausar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pausar'");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'selecionarMusica'");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'exibirPagina'");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarNovaAba'");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizarPagina'");
+    }
+
+    @Override
+    public void ligar(String numero) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+    }
+
+    @Override
+    public void atender() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atender'");
+    }
+
+    @Override
+    public void IniciarCorreioVoz() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'IniciarCorreioVoz'");
+    }
+}
