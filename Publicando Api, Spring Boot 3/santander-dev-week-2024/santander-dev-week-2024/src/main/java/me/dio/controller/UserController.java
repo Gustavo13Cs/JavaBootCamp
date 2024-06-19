73e5d884cb443f838b87fc831162bede3531a5a7
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-06-19 18:13:09 -0300

    .

--- Publicando Api, Spring Boot 3/santander-dev-week-2024/santander-dev-week-2024/src/main/java/me/dio/controller/UserController.java
+++ Publicando Api, Spring Boot 3/santander-dev-week-2024/santander-dev-week-2024/src/main/java/me/dio/controller/UserController.java
@@ -2,7 +2,6 @@ package me.dio.controller;
 
 import java.net.URI;
 
-import org.springframework.boot.autoconfigure.jersey.JerseyProperties.Servlet;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
